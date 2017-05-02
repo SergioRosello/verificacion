03fde801dba3d9e909057c0558dfea959cc163f0
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2017-05-02 22:39:53 +0200

    Finished first part of project (needs testing with selenium)

--- code/__init__.py
+++ code/__init__.py
@@ -10,23 +10,33 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    errors = []
-    results = {}
+    results = []
     if request.method == "POST":
+
+        # TODO: Menuda mierda!! (a ver si se puede hacer mejor (por ahora funciona))
+        if request.form['text-box'] == '':
+            return '', 204
+
         # get text that the person has entered
         text = request.form['text-box']
         # text processing
         analyzer = TextAnalyzer(text)
-        phrase = analyzer.text_analyzer()
-        # save the results
-        results = sorted(phrase.items(),key=operator.itemgetter(1),reverse=True)
+        result = analyzer.text_analyzer()
+        phrase = result[1]
+        for word in sorted(phrase, key=phrase.get, reverse=True):
+            aux = []
+            aux.append(word)
+            aux.append(phrase[word])
+            results.append(aux)
+
         try:
             dbconnection = DBConnection()
-            dbconnection.save_in_database(phrase)
+            dbconnection.save_in_database(result[0])
             dbconnection.query()
         except:
-            errors.append("Unable to add item to database.")
-    return render_template('index.html', errors=errors, results=results)
+            print "Unable to add item to database."
+
+    return render_template('index.html', results=results)
 
 
 if __name__ == "__main__":
